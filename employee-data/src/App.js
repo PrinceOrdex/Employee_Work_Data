2d6bc4a3388230df7ee148219308b0d7801abd08
PrinceOrdex <prince.talaviya@ordextechnology.com>
2022-08-24 22:19:46 +0530

    .

--- employee-data/src/App.js
+++ employee-data/src/App.js
@@ -5,21 +5,20 @@ function App() {
   return (
     <>
       <div className="container-fluid heading">
-        <h3 className='py-4 text-center'>Employee Data</h3>
+        <h3 className='py-4 text-center mb-0'>Employee Task Data</h3>
       </div>
       <div className='main'>
-
-
-        <div className="container">
-          <form action="">
+        <div className="container vh-100">
+          <form action="" className='pt-4'>
             <div class="form-group">
               <label for="email">Email address</label>
-              <input type="text" className="form-control" id="email" aria-describedby="emailHelp" />
+              <input type="text" className="form-control" id="email" aria-describedby="emailHelp" required />
             </div>
             <div class="form-group">
-              <input type="date" className="form-control" id="date" aria-describedby="date" />
+              <label for="date">Date</label>
+              <input type="date" className="form-control" id="date" aria-describedby="date" required />
             </div>
-            <h5>Enter Your Tasks</h5>
+            <h5 className='text-center mt-5'>Enter Your Tasks</h5>
 
             <table class="table">
               <thead class="thead-light">
@@ -32,13 +31,16 @@ function App() {
               </thead>
               <tbody>
                 <tr>
-                  <th scope="row">1</th>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
+                  <th scope="row" className='px-0 pr-1'><input type="number" className='w-100 py-2 px-0 border-0 border-right' required /></th>
+                  {/* <td className='px-0 pr-1'><input type="text" className='w-100 py-2 px-0 border-0' /></td> */}
+                  <td className='px-0 pr-1'><textarea name="" id="" rows="1" className='w-100 py-2 border-0 required'></textarea></td>
+                  {/* <td className='px-0 pr-1'><input type="text" className='w-100 py-2 border-0' /></td> */}
+                  <td className='px-0 pr-1'><textarea name="" id="" rows="1" className='w-100 py-2 border-0 required'></textarea></td>
+                  <td className='px-0'><input type="text" className='w-100 py-2 border-0' /></td>
                 </tr>
               </tbody>
             </table>
+            <button type="submit">Submit</button>
           </form>
         </div>
       </div>
